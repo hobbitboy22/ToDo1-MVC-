c783a86d4d08264eabffa3db99c12925c4070899
hobbitboy22 <hobbitboy22@gmail.com>
2023-02-07 11:43:36 +0000

    Add project files.

---
+++ ToDo1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToDo1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
